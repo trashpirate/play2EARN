5f7a2fda9931869150db54cff4b0ab41481fb22b
trashpirate <nadinaoates@gmail.com>
2023-10-27 00:43:02 -0500

    ready for mainnet

--- frontend/src/components/claimPrize/claimPrize.tsx
+++ frontend/src/components/claimPrize/claimPrize.tsx
@@ -1,91 +1,12 @@
-"use client";
-import React, { useEffect, useState } from "react";
-import { useContractRead, useNetwork } from "wagmi";
-
-import { tokenABI } from "@/assets/tokenABI";
-import { nftABI } from "@/assets/nftABI";
-
-import Image from "next/image";
 import CopyToClipboard from "../copyToClipboard";
-import {Alchemy, Network, NftMetadataBatchToken} from "alchemy-sdk";
-const NFT_CONTRACT = process.env.NEXT_PUBLIC_NFT_CONTRACT as `0x${string}`;
-const TOKEN_CONTRACT = process.env.NEXT_PUBLIC_TOKEN_CONTRACT as `0x${string}`;
-const CLAIM_ADDRESS = "0x0cf66382d52C2D6c1D095c536c16c203117E2B2f";
-
+const CLAIM_ADDRESS = "0xb75D371Fad387ebc95F80D278208E442CAa4a2E3";
 
 type Props = {};
 
-const config = {
-  apiKey: process.env.NEXT_PUBLIC_ALCHEMY_API_KEY,
-  network:
-    process.env.NEXT_PUBLIC_TESTNET == "true"
-      ? Network.ETH_GOERLI
-      : Network.ETH_MAINNET,
-};
-
-const alchemy = new Alchemy(config);
-
 export default function ClaimPrize({}: Props) {
-  const [totalSupply, setTotalSupply] = useState<number | undefined>(undefined);
-
-  // get chain
-  const { chain } = useNetwork();
-
-  // define token contract config
-  const tokenContract = {
-    address: TOKEN_CONTRACT,
-    abi: tokenABI,
-    chainId: chain?.id,
-  };
-
-  // define token contract config
-  const nftContract = {
-    address: NFT_CONTRACT,
-    abi: nftABI,
-    chainId: chain?.id,
-  };
-
-  // read current limits
-  const { data, isSuccess, isError, isLoading } = useContractRead({
-    ...nftContract,
-    functionName: "totalSupply",
-    watch: true,
-    cacheTime: 1000,
-  });
-
-  useEffect(() => {    
-    if (data != undefined) {
-      setTotalSupply(Number(data));
-    }
-  }, [data]);
-
-  function getTotalSupplyString() {
-    let text: string = "---";
-    if (isLoading) {
-      text = "Loading...";
-    } else if (isSuccess && totalSupply != undefined) {
-      text = `${totalSupply}`;
-    } else {
-      text = "---";
-    }
-    return text;
-  }
-
-  function getNftsRemainingString() {
-    let text: string = "---";
-    if (isLoading) {
-      text = "Loading...";
-    } else if (isSuccess && totalSupply != undefined) {
-      text = `${100 - totalSupply}`;
-    } else {
-      text = "---";
-    }
-    return text;
-  }
-
   return (
     <div className="mx-auto w-full">
-      <div className="mx-auto max-w-sm rounded-md bg-black p-8  md:max-w-none shadow-inner-sym">
+      <div className="mx-auto max-w-sm rounded-md bg-black p-8  shadow-inner-sym md:max-w-none">
         <h2 className="mb-4 border-b-2 border-yellow-500 pb-2 text-xl uppercase">
           Claim your prize
         </h2>
@@ -94,9 +15,9 @@ export default function ClaimPrize({}: Props) {
           <CopyToClipboard text={CLAIM_ADDRESS} copyText={CLAIM_ADDRESS} />
         </div>
         <div>
-            To claim your prize, send your winning NFT Cards to the address above and you will receive your prize within 24hrs.
+          To claim your prize, send your winning NFT Cards to the address above
+          and you will receive your prize within 24hrs.
         </div>
-        
       </div>
     </div>
   );
